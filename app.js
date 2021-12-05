0611ac2a6104fcf3593dc335b86e8568ff71c9d7
Mohamed Hosam <79701103+m4hosam@users.noreply.github.com>
2021-12-05 22:36:42 +0300

    First responsive version
    All features has been added successfully without bugs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const array = document.querySelectorAll(".thumbnails");
 const productNum = document.querySelector("#span-product-no");
 let width = document.documentElement.clientWidth;
 
+// side bar js
 document.getElementById("clickSideBar").addEventListener("click", function () {
     const links = document.getElementById("links");
     const cloneLinks = links.cloneNode(true)
@@ -25,11 +26,18 @@ document.getElementById("clickSideBar").addEventListener("click", function () {
 });
 
 
-
+//  Listing to the subimages click
+let flag = 0;
 for (let i = 0; i < array.length; i++) {
     array[i].addEventListener("click", function () {
         image.src = `images/image-product-${i + 1}.jpg`;
         image.alt = i + 1;
+        array[i].style.outline = ".2rem solid hsl(26, 100%, 55%)";
+        array[i].style.opacity = ".5";
+        // removing the selection from the previos element
+        array[flag].style.opacity = "1";
+        array[flag].style.outline = "0";
+        flag = i;
     });
 }
 
@@ -84,11 +92,19 @@ image.addEventListener("click", function () {
             a[i].style.width = "15%"
 
         const b = document.querySelector(".product1");
+        let flag2 = flag;
+
+
         arrowRight.addEventListener("click", function () {
             let z = parseInt(b.alt);
             if (z + 1 == 5)
                 z = 0
             b.src = `images/image-product-${z + 1}.jpg`;
+            a[z].style.outline = ".2rem solid hsl(26, 100%, 55%)";
+            a[z].style.filter = "contrast(.5)";
+            a[flag2].style.outline = "0";
+            a[flag2].style.filter = "contrast(1)";
+            flag2 = z;
             b.alt = z + 1;
         })
         arrowLeft.addEventListener("click", function () {
@@ -96,6 +112,11 @@ image.addEventListener("click", function () {
             if (z - 1 == 0)
                 z = 5
             b.src = `images/image-product-${z - 1}.jpg`;
+            a[z - 2].style.outline = ".2rem solid hsl(26, 100%, 55%)";
+            a[z - 2].style.filter = "contrast(.5)";
+            a[flag2].style.outline = "0";
+            a[flag2].style.filter = "contrast(1)";
+            flag2 = z - 2;
             b.alt = z - 1;
         })
 
