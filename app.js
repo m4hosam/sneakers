a2ff72ff6e61423959a41637fd4e4380c5f0f784
Mohamed Hosam <79701103+m4hosam@users.noreply.github.com>
2021-12-05 15:48:20 +0300

    Adding the slider to phone size
    Missing some refactoring to the code

--- app.js
+++ app.js
@@ -26,53 +26,56 @@ for (let i = 0; i < array.length; i++) {
     });
 }
 
+
+
+
 image.addEventListener("click", function () {
-    const images = document.querySelector(".images");
-    let arrowRight = document.createElement("span");
-    let arrowLeft = document.createElement("span");
-    let clonedImages = images.cloneNode(true);
-    let sliderBG = document.createElement("div");
+    let width = document.documentElement.clientWidth;
+    if (width >= 900) {
+        const images = document.querySelector(".images");
+        const arrowRight = document.querySelector(".arrow-right");
+        const arrowLeft = document.querySelector(".arrow-left");
+        const clonedImages = images.cloneNode(true);
+        const sliderBG = document.createElement("div");
+        const arrows = document.querySelectorAll(".arrow");
 
-    document.body.prepend(sliderBG);
-    sliderBG.classList = 'image-slider-BG';
+        document.body.prepend(sliderBG);
+        sliderBG.classList = 'image-slider-BG';
 
-    document.body.prepend(clonedImages);
-    clonedImages.classList = "images-center";
+        document.body.prepend(clonedImages);
+        clonedImages.classList = "images-center";
 
-    images.prepend(arrowRight);
-    arrowRight.innerHTML = "&#8250;";
-    arrowRight.classList = 'arrow arrow-right'
+        for (let e of arrows)
+            e.style.display = "block";
 
-    images.prepend(arrowLeft);
-    arrowLeft.innerHTML = "&#8249;";
-    arrowLeft.classList = 'arrow arrow-left'
+        document.querySelector(".sub-images").style.justifyContent = "space-evenly";
+        const a = document.querySelectorAll(".thumbnails");
 
-    document.querySelector(".sub-images").style.justifyContent = "space-evenly";
-    let a = document.querySelectorAll(".thumbnails");
+        for (let i = 0; i < 4; i++)
+            a[i].style.width = "15%"
 
-    for (let i = 0; i < 4; i++)
-        a[i].style.width = "15%"
+        const b = document.querySelector(".product1");
+        arrowRight.addEventListener("click", function () {
+            let z = parseInt(b.alt);
+            if (z + 1 == 5)
+                z = 0
+            b.src = `images/image-product-${z + 1}.jpg`;
+            b.alt = z + 1;
+        })
+        arrowLeft.addEventListener("click", function () {
+            let z = parseInt(b.alt);
+            if (z - 1 == 0)
+                z = 5
+            b.src = `images/image-product-${z - 1}.jpg`;
+            b.alt = z - 1;
+        })
 
-    const b = document.querySelector(".product1");
-    arrowRight.addEventListener("click", function () {
-        let z = parseInt(b.alt);
-        if (z + 1 == 5)
-            z = 0
-        b.src = `images/image-product-${z + 1}.jpg`;
-        b.alt = z + 1;
-    })
-    arrowLeft.addEventListener("click", function () {
-        let z = parseInt(b.alt);
-        if (z - 1 == 0)
-            z = 5
-        b.src = `images/image-product-${z - 1}.jpg`;
-        b.alt = z - 1;
-    })
 
-    sliderBG.addEventListener("click", function () {
-        clonedImages.remove();
-        sliderBG.remove();
-        arrowLeft.remove();
-        arrowRight.remove();
-    });
+        sliderBG.addEventListener("click", function () {
+            clonedImages.remove();
+            sliderBG.remove();
+            for (let e of arrows)
+                e.style.display = "none";
+        });
+    }
 })
