22f58a2b1cad4c4bcaff8997654764fc1d8fc875
Mohamed Hosam <79701103+m4hosam@users.noreply.github.com>
2021-12-05 16:32:51 +0300

    Fixing the slider bugs

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const arrowRight = document.querySelector(".arrow-right");
+const arrowLeft = document.querySelector(".arrow-left");
+const arrows = document.querySelectorAll(".arrow");
+const image = document.querySelector(".product1");
+const array = document.querySelectorAll(".thumbnails");
+const productNum = document.querySelector("#span-product-no");
+let width = document.documentElement.clientWidth;
+
 document.getElementById("clickSideBar").addEventListener("click", function () {
     const links = document.getElementById("links");
     const cloneLinks = links.cloneNode(true)
@@ -16,8 +24,7 @@ document.getElementById("clickSideBar").addEventListener("click", function () {
     });
 });
 
-let image = document.querySelector(".product1");
-let array = document.querySelectorAll(".thumbnails");
+
 
 for (let i = 0; i < array.length; i++) {
     array[i].addEventListener("click", function () {
@@ -28,16 +35,38 @@ for (let i = 0; i < array.length; i++) {
 
 
 
+arrowRight.addEventListener("click", function () {
+    width = document.documentElement.clientWidth;
+    if (width <= 900) {
+        let z = parseInt(image.alt);
+        if (z + 1 == 5)
+            z = 0
+        image.src = `images/image-product-${z + 1}.jpg`;
+        image.alt = z + 1;
+    }
+
+})
+arrowLeft.addEventListener("click", function () {
+    width = document.documentElement.clientWidth;
+    if (width <= 900) {
+        let z = parseInt(image.alt);
+        if (z - 1 == 0)
+            z = 5
+        image.src = `images/image-product-${z - 1}.jpg`;
+        image.alt = z - 1;
+    }
+})
+
+
+
 
 image.addEventListener("click", function () {
-    let width = document.documentElement.clientWidth;
+    width = document.documentElement.clientWidth;
     if (width >= 900) {
         const images = document.querySelector(".images");
-        const arrowRight = document.querySelector(".arrow-right");
-        const arrowLeft = document.querySelector(".arrow-left");
         const clonedImages = images.cloneNode(true);
         const sliderBG = document.createElement("div");
-        const arrows = document.querySelectorAll(".arrow");
+
 
         document.body.prepend(sliderBG);
         sliderBG.classList = 'image-slider-BG';
@@ -78,4 +107,16 @@ image.addEventListener("click", function () {
                 e.style.display = "none";
         });
     }
+})
+
+document.querySelector(".add").addEventListener("click", function () {
+    let n = parseInt(productNum.textContent)
+    productNum.innerHTML = n + 1;
+})
+document.querySelector(".reduce").addEventListener("click", function () {
+    let n = parseInt(productNum.textContent)
+    if (n != 0) {
+        productNum.innerHTML = n - 1;
+    }
+
 })
